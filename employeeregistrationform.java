9331f8fe86105ce822b4a2529c8c5f1d08ba5219
mayur-sawant <64126556+mayur-sawant@users.noreply.github.com>
2023-02-28 01:08:29 +0530

    Create employeeregistrationform.java

---
+++ employeeregistrationform.java
@@ -0,0 +1,92 @@
+import java.sql.*;
+
+public class EmployeeEx {
+    private int id;
+    private int age;
+    private String firstName;
+    private String lastName;
+    private String contactNumber;
+    private String address;
+    private String email;
+    private String department;
+    private int salary;
+
+    public EmployeeEx(int id, int age, String firstName, String lastName, String contactNumber, String address,String email, String department, int salary) {
+        this.id = id;
+        this.age = age;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.contactNumber = contactNumber;
+        this.address = address;
+        this.email = email;
+        this.department = department;
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getContactNumber() {
+        return contactNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public static void insertEmployee(EmployeeEx emp) {
+        String url = "jdbc:mysql://localhost:3306/employeedb";
+        String user = "root";
+        String password = "admin";
+        String sql = "INSERT INTO employees (id, age, first_name, last_name, contact_number,address, email, department, salary) " + "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        try (Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/employeedb", "root", "admin");
+                PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setInt(1, emp.getId());
+            stmt.setInt(2, emp.getAge());
+            stmt.setString(3, emp.getFirstName());
+            stmt.setString(4, emp.getLastName());
+            stmt.setString(5, emp.getContactNumber());
+            stmt.setString(6, emp.getAddress());
+            stmt.setString(7, emp.getEmail());
+            stmt.setString(8, emp.getDepartment());
+            stmt.setInt(9, emp.getSalary());
+            int rowsInserted = stmt.executeUpdate();
+            if (rowsInserted > 0) {
+                System.out.println("Employee record inserted successfully.");
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        EmployeeEx emp = new EmployeeEx(1, 25, "John", "Doe", "1234567890", "123 Main St", "johndoe@example.com", "IT",50000);
+        EmployeeEx.insertEmployee(emp);
+    }
+}
+
